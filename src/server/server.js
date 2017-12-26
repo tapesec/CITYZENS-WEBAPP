ba4056bf583ccfb8c708a8b1db299a8a316d6e63
lasalle <lionel.dupouy@gmail.com>
2017-12-26 23:20:10 +0100

    chore: add auth0 hosted pages

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,72 @@
 import express from 'express';
 const app = express();
-
 import router from './router';
+import passport from 'passport';
+import Auth0Strategy from 'passport-auth0';
+import session from 'express-session';
+
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {}
+    })
+);
+// Configure Passport to use Auth0
+const strategy = new Auth0Strategy(
+    {
+        domain: 'cityzens.eu.auth0.com',
+        clientID: 'n131Yxuh4Z5pCHz8tj5ZZkWybvyqnJQd',
+        clientSecret:
+            '_zYuOp0VDjNjz4ivkKxsp1wYCWGWveRu4r6r1pJ9VMcIlE7n5C7v3L95uQf5yK_F',
+        callbackURL: 'http://localhost:3000/callback'
+    },
+    (accessToken, refreshToken, extraParams, profile, done) => {
+        return done(null, accessToken);
+    }
+);
+passport.use(strategy);
+
+// This can be used to keep a smaller payload
+passport.serializeUser(function(accessToken, done) {
+    done(null, { accessToken });
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+// ...
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/assets', express.static('build'));
-app.get('*', router);
+
+app.get(
+    '/login',
+    passport.authenticate('auth0', {
+        clientID: 'n131Yxuh4Z5pCHz8tj5ZZkWybvyqnJQd',
+        domain: 'cityzens.eu.auth0.com',
+        redirectUri: 'http://localhost:3000/callback',
+        audience: 'https://cityzens.eu.auth0.com/userinfo',
+        responseType: 'code',
+        scope: 'openid profile'
+    }),
+    function(req, res) {
+        res.redirect('/');
+    }
+);
+app.get(
+    '/callback',
+    passport.authenticate('auth0', {
+        failureRedirect: '/'
+    }),
+    function(req, res) {
+        res.redirect('/');
+    }
+);
+app.get(['/register', '/'], router);
 
 app.listen(3000, () => {
     console.log('ready to serve pages');
