d548569a46686ecfd7a4188542627a1e844adeea
lasalle <lionel.dupouy@gmail.com>
2017-12-28 08:35:28 +0100

    style: remove body margin and try to make it work this Material f** Drawer

--- src/shared/components/dashboard/LeftSideMenu.js
+++ src/shared/components/dashboard/LeftSideMenu.js
@@ -1,11 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { PersistentDrawer, PersistentDrawerHeader, PersistentDrawerContent } from 'rmwc/Drawer';
+import { ListItem, ListItemText } from 'rmwc/List';
+import { Fab } from 'rmwc/Fab';
 
-export default function LeftSideMenu() {
-    return (
-        <div>
+export default class LeftSideMenu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: true,
+        };
+    }
+
+    render() {
+        return (
+            /* <div>
             <h1>Menu de gauche</h1>
             <Link to="/martignas/Mairie">Mairie de Martignas</Link>
-        </div>
-    );
+        </div> */
+            <div>
+                <Fab mini onClick={() => this.setState({ open: !this.state.open })}>
+                    favorite
+                </Fab>
+                <PersistentDrawer>
+                    <PersistentDrawerHeader
+                        open={this.state.open}
+                        onClose={() => this.setState({ open: false })}
+                        style={{ backgroundColor: 'blue' }}>
+                        PersistentDrawerHeader
+                    </PersistentDrawerHeader>
+                    <PersistentDrawerContent>
+                        <ListItem>
+                            <ListItemText>Cookies</ListItemText>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText>Pizza</ListItemText>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText>Icecream</ListItemText>
+                        </ListItem>
+                    </PersistentDrawerContent>
+                </PersistentDrawer>
+            </div>
+        );
+    }
 }
