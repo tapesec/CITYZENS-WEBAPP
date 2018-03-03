2f36be1434c3ef4eaeca487c21ea73b8599bc06f
lasalle <lionel.dupouy@gmail.com>
2018-03-03 07:21:21 +0100

    feat: add default message in WallHotspot when no messages was still created by author

--- src/shared/components/dashboard/HotspotContainer/HotspotViewMod/WallHotspot.js
+++ src/shared/components/dashboard/HotspotContainer/HotspotViewMod/WallHotspot.js
@@ -4,17 +4,38 @@ import CustomScroll from 'react-custom-scroll';
 import HotspotTitle from './HotspotTitle';
 import HotspotMessagesWall from './HotspotMessagesWall';
 import HotspotMessage from './HotspotMessage';
+import {
+    DEFAULT_HOTSPOT_MESSAGES_WORDING_BODY,
+    DEFAULT_HOTSPOT_MESSAGES_WORDING_TITLE,
+    CITYZEN_ADMIN_NAME,
+} from './../../../../wording';
 
 import './HotspotContent.scss';
+
+const EMPTY_MESSAGE_KEY = 'first-key';
+const EMPTY_MESSAGE_WORDING = {
+    id: EMPTY_MESSAGE_KEY,
+    title: DEFAULT_HOTSPOT_MESSAGES_WORDING_TITLE,
+    author: {
+        pseudo: CITYZEN_ADMIN_NAME,
+    },
+    createdAt: new Date().toISOString(),
+    updatedAt: undefined,
+    body: DEFAULT_HOTSPOT_MESSAGES_WORDING_BODY,
+};
 
 const WallHotspot = ({ hotspot }) => (
     <section className="HotspotContent">
         <HotspotTitle title={hotspot.title} />
         <CustomScroll heightRelativeToParent="100%">
             <HotspotMessagesWall>
-                {hotspot.messages.map(message => (
-                    <HotspotMessage message={message} key={message.id} />
-                ))}
+                {hotspot.messages.length === 0 ? (
+                    <HotspotMessage message={EMPTY_MESSAGE_WORDING} key={EMPTY_MESSAGE_KEY} />
+                ) : (
+                    hotspot.messages.map(message => (
+                        <HotspotMessage message={message} key={message.id} />
+                    ))
+                )}
             </HotspotMessagesWall>
         </CustomScroll>
     </section>
---
+++ src/shared/wording/index.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line
+export const DEFAULT_HOTSPOT_MESSAGES_WORDING_TITLE = `
+Exprimez vous !
+`;
+export const DEFAULT_HOTSPOT_MESSAGES_WORDING_BODY = `
+Aucun message n'a encore été rédigé pour ce centre d'interet...
+`;
+export const CITYZEN_ADMIN_NAME = 'L\'équipe Cityzens';
