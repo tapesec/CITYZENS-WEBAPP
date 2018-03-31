572ffa53192da5cabe5b19d1a5a561b928de8257
Lasalle <lionel.dupouy@gmail.com>
2018-03-31 19:32:37 +0200

    feat: update index title

--- src/server/renderFullPage.js
+++ src/server/renderFullPage.js
@@ -4,7 +4,7 @@ export default function renderFullPage(html, preloadedState) {
 		<head>
 			<meta charset="utf-8" />
 			<meta http-equiv="X-UA-Compatible" content="IE=9; IE=edge">
-			<title>Page Title</title>
+			<title>Cityzens Martignas sur Jalle</title>
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<link href="/assets/styles.css" rel="stylesheet">
 			<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
