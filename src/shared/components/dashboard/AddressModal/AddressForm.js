0218c353a5580c44ff8361fb31589f08f84c9b3b
Lasalle <lionel.dupouy@gmail.com>
2018-05-18 15:48:59 +0200

    refactor: form validation works with multiple fields

--- src/shared/components/dashboard/AddressModal/AddressForm.js
+++ src/shared/components/dashboard/AddressModal/AddressForm.js
@@ -23,81 +23,136 @@ const validateAddress = values => {
     return errors;
 };
 
+const validateEmail = values => {
+    const errors = {
+        isValid: true,
+        messages: [],
+    };
+    if (!values.email) {
+        errors.isValid = false;
+        errors.messages.push(VALIDATION.ALL.LABEL.ERROR);
+    }
+    if (values.email && values.email.length > 2) {
+        errors.isValid = false;
+        errors.messages.push(VALIDATION.HOTSPOT.ADDRESS.LABEL.ERROR);
+    }
+    return errors;
+};
+
+const validateAndUpdateFieldStateOnChange = (formState, fieldName, fieldValidator, inputValue) => {
+    const newState = {};
+    newState.formValues = {
+        ...formState.formValues,
+        [fieldName]: inputValue,
+    };
+    if (formState.validate[fieldName] && formState.validate[fieldName].touched) {
+        const validateObject = fieldValidator(newState.formValues);
+        newState.validate = {
+            ...formState.validate,
+            [fieldName]: {
+                touched: true,
+                ...validateObject,
+            },
+        };
+    }
+    return newState;
+};
+
+const validateAndUpdateFieldStateOnBlur = (formState, fieldName, fieldValidator) => {
+    const newState = {};
+    const validateObject = fieldValidator(formState.formValues);
+    const newFieldStatus = {
+        ...validateObject,
+        touched: true,
+    };
+    newState.validate = {
+        ...formState.validate,
+        [fieldName]: newFieldStatus,
+    };
+    return newState;
+};
+
+const listFieldsAndValidators = [
+    {
+        name: 'address',
+        validator: validateAddress,
+    },
+    {
+        name: 'email',
+        validator: validateEmail,
+    },
+];
+
+const formStatusBeforeSubmit = (formState, fieldsAndValidators) => {
+    const newFormState = { ...formState };
+    fieldsAndValidators.forEach(currentField => {
+        const validateObject = currentField.validator(formState.formValues);
+        newFormState.validate[currentField.name] = {
+            ...validateObject,
+            touched: true,
+        };
+    });
+    const isInvalid = fieldsAndValidators.some(currentField => {
+        const validateObject = currentField.validator(formState.formValues);
+        return validateObject.isValid === false;
+    });
+    return {
+        isValid: !isInvalid,
+        newStateToUpdate: newFormState,
+    };
+};
+
 class AddressForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             formValues: {
                 address: props.initialValues.address || '',
+                email: props.initialValues.email || '',
             },
             validate: {},
         };
         this.fieldConnector = this.fieldConnector.bind(this);
         this.initValidationField = this.initValidationField.bind(this);
         this.formSubmit = this.formSubmit.bind(this);
-        this.validateAndUpdateField = this.validateAndUpdateField.bind(this);
-    }
-
-    validateAndUpdateField(fieldName, fieldValidator, inputValue) {
-        const newState = {};
-        newState.formValues = {
-            [fieldName]: inputValue,
-        };
-        if (this.state.validate[fieldName] && this.state.validate[fieldName].touched) {
-            const validateObject = fieldValidator(newState.formValues);
-            newState.validate = {
-                [fieldName]: {
-                    touched: true,
-                    ...validateObject,
-                },
-            };
-        }
-        return newState;
     }
 
     fieldConnector(fieldName, fieldValidator) {
         return evt => {
-            const newState = this.validateAndUpdateField(
+            const newState = validateAndUpdateFieldStateOnChange(
+                this.state,
                 fieldName,
                 fieldValidator,
                 evt.target.value,
             );
+            console.log(newState, 'newState on change');
             this.setState(newState);
         };
     }
 
     initValidationField(fieldName, fieldValidator) {
         return () => {
-            const validateObject = fieldValidator(this.state.formValues);
-            this.setState({
-                validate: {
-                    [fieldName]: {
-                        touched: true,
-                        ...validateObject,
-                    },
-                },
-            });
+            const newState = validateAndUpdateFieldStateOnBlur(
+                this.state,
+                fieldName,
+                fieldValidator,
+            );
+            console.log(newState, 'newState after blur');
+            this.setState(newState);
         };
     }
 
     formSubmit(evt) {
-        const { formValues } = this.state;
-        const isInvalid = [
-            {
-                name: 'address',
-                validator: validateAddress,
-            },
-        ].some(currentField => {
-            this.initValidationField(currentField.name, currentField.validator)();
-            const validateObject = currentField.validator(formValues);
-            return validateObject.isValid === false;
-        });
-        if (isInvalid) {
+        // function may starts here
+        const formStatus = formStatusBeforeSubmit(this.state, listFieldsAndValidators);
+        console.log(this.state, 'this.state');
+        if (formStatus.isValid === false) {
             evt.preventDefault();
+            this.setState(formStatus.newStateToUpdate);
             return false;
         }
         this.props.onSubmit(this.state.formValues);
-        return false;
+        return true;
     }
 
     render() {
@@ -144,6 +199,32 @@ class AddressForm extends React.Component {
                                   </TextFieldHelperText>
                               ))
                             : null}
+                        <TextField
+                            className="cyz-text-field"
+                            theme="text-on-primary-background"
+                            label={inputLabel}
+                            value={this.state.formValues.email}
+                            onChange={this.fieldConnector('email', validateEmail)}
+                            onBlur={this.initValidationField('email', validateEmail)}
+                            invalid={
+                                this.state.validate.email && !this.state.validate.email.isValid
+                            }
+                        />
+                        {this.state.validate.email && this.state.validate.email.isValid === false
+                            ? this.state.validate.email.messages.map(message => (
+                                  <TextFieldHelperText
+                                      style={{ color: 'red' }}
+                                      validationMsg
+                                      key={message}>
+                                      <Icon
+                                          strategy="ligature"
+                                          style={{ verticalAlign: 'middle', fontSize: '0.75rem' }}>
+                                          close
+                                      </Icon>
+                                      {message}
+                                  </TextFieldHelperText>
+                              ))
+                            : null}
                     </GridCell>
                     <GridCell span="6" phone="12" tablet="12">
                         <Button type="submit" raised theme="secondary-bg text-primary-on-secondary">
@@ -172,6 +253,7 @@ AddressForm.propTypes = {
     inputLabel: PropTypes.string.isRequired,
     initialValues: PropTypes.shape({
         address: PropTypes.string,
+        email: PropTypes.string,
     }),
     meta: PropTypes.object, // eslint-disable-line
 };
@@ -179,6 +261,7 @@ AddressForm.propTypes = {
 AddressForm.defaultProps = {
     initialValues: {
         address: 'test',
+        email: 'bof',
     },
 };
 
