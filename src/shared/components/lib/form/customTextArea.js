164005cef66252e3d1b27dbe521239ea4c284bc9
Lasalle <lionel.dupouy@gmail.com>
2018-07-12 06:42:25 +0200

    DDay: WIP

---
+++ src/shared/components/lib/form/customTextArea.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class CustomTextArea extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+}
+
+export default CustomTextArea;
