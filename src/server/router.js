391f7eeadb1267ec6f3c0c3fda89ddcee48c5b5d
Lasalle <lionel.dupouy@gmail.com>
2018-07-26 08:35:25 +0200

    change ssr render method

--- src/server/router.js
+++ src/server/router.js
@@ -1,4 +1,4 @@
-import { renderToNodeStream } from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import React from 'react';
 import { matchPath, StaticRouter } from 'react-router-dom';
 import { createStore } from 'redux';
@@ -24,7 +24,7 @@ export default (async function router(req, res) {
 
     const context = {};
 
-    const html = renderToNodeStream(
+    const html = renderToString(
         <StaticRouter context={context} location={req.url}>
             <Provider store={store}>
                 <App />
