4116cc49dfbb7099a163f5174c74a4364f228490
Lasalle <lionel.dupouy@gmail.com>
2018-08-12 12:32:40 +0200

    analytics: add GA balise

--- src/server/renderFullPage.js
+++ src/server/renderFullPage.js
@@ -10,6 +10,14 @@ export default function renderFullPage(html, preloadedState) {
 			<link href="/assets/styles.css" rel="stylesheet">
 			<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 			<link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro" rel="stylesheet">
+			<!-- Global site tag (gtag.js) - Google Analytics -->
+			<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123804129-1"></script>
+			<script>
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+				gtag('config', 'UA-123804129-1');
+			</script>
 		</head>
 		<body>
 			<div id="root">${html}</div>
